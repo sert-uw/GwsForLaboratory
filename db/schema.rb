9deb779e2c14f19d6c62653c1fc5d1d8d76c1f98
sert <demo.sert@gmail.com>
2015-06-17 22:39:35 +0900

    add name column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617130011) do
+ActiveRecord::Schema.define(version: 20150617133908) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20150617130011) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "name",                   limit: 255
+    t.integer  "grade",                  limit: 4
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
