69aaee8ed9a0303b81a70a56e9f15fc6ec5daae3
sert <demo.sert@gmail.com>
2015-06-20 20:20:14 +0900

    add has_and_belongs_to_many to group.rb and user.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,6 @@ class Group < ActiveRecord::Base
   belongs_to :parent, class_name: "Group"
 
   has_many :children, class_name: "Group", dependent: :destroy
+
+  has_and_belongs_to_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620103346) do
+ActiveRecord::Schema.define(version: 20150620111405) do
+
+  create_table "group_users", force: :cascade do |t|
+    t.integer  "group_id",   limit: 4
+    t.integer  "user_id",    limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -19,6 +26,11 @@ ActiveRecord::Schema.define(version: 20150620103346) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "group_id",   limit: 4
+  end
+
+  create_table "groups_users", force: :cascade do |t|
+    t.integer "group_id", limit: 4
+    t.integer "user_id",  limit: 4
   end
 
   create_table "users", force: :cascade do |t|
