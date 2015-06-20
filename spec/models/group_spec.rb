4c50d5068d3c1b4607be5363d4ad91ea9f325d71
sert <demo.sert@gmail.com>
2015-06-20 21:05:31 +0900

    add test (group has many users)

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -11,4 +11,35 @@ RSpec.describe Group, type: :model do
     let(:parent) { create :group }
     it { expect(parent.children.create(name: 'Children')).not_to eq nil }
   end
+
+  describe "include users" do
+    let(:group) { create :group }
+    let(:user) { create :user }
+    subject { group.users.count }
+
+    describe "no user" do
+      it { expect(subject).to eq 0 }
+    end
+
+    describe "has one user" do
+      it "create by group.users.create" do
+        group.users.create(name: 'user1', email: 'test1@test.test', password: 'password1')
+        expect(subject).to eq 1
+      end
+
+      it "add user to group.users" do
+        group.users << user
+        expect(subject).to eq 1
+      end
+    end
+
+    describe "has many users" do
+      before do
+        group.users.create(name: 'user1', email: 'test1@test.test', password: 'password1')
+        group.users << user
+      end
+
+      it { expect(subject).to eq 2 }
+    end
+  end
 end
