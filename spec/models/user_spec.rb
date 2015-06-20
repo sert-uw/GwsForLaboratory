a085351e271c153c8188b75526447e3f5fcbda38
sert <demo.sert@gmail.com>
2015-06-20 21:11:12 +0900

    add test (user has many groups)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,35 @@ RSpec.describe User, type: :model do
     it { expect(build(:user, email: nil)).not_to be_valid }
     it { expect(build(:user, password: nil)).not_to be_valid }
   end
+
+  describe "include groups" do
+    let(:group) { create :group }
+    let(:user) { create :user }
+    subject { user.groups.count }
+
+    describe "no group" do
+      it { expect(subject).to eq 0 }
+    end
+
+    describe "has one group" do
+      it "create by user.groups.create" do
+        user.groups.create(name: 'Group1', category: 'cat1')
+        expect(subject).to eq 1
+      end
+
+      it "add group to user.groups" do
+        user.groups << group
+        expect(subject).to eq 1
+      end
+    end
+
+    describe "has many groups" do
+      before do
+        user.groups.create(name: 'Group1', category: 'cat1')
+        user.groups << group
+      end
+
+      it { expect(subject).to eq 2 }
+    end
+  end
 end
