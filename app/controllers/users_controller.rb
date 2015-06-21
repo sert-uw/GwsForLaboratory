3af326805d9f12faea6217283236417b31724b27
sert <demo.sert@gmail.com>
2015-06-21 19:12:17 +0900

    create edit view of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -11,6 +11,18 @@ class UsersController < ApplicationController
   def show
   end
 
+  def edit
+    @groups = Group.all
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: t('notice.users.update')
+    else
+      redirect_to @user.errors, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @user.destroy
     redirect_to users_path, notice: t('notice.users.delete')
@@ -18,7 +30,11 @@ class UsersController < ApplicationController
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :group_ids => [])
+    end
 end
