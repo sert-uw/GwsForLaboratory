bbc11541516e7cc10c20b607740afbbff73f5ff3
sert <demo.sert@gmail.com>
2015-06-21 18:39:43 +0900

    add destroy method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :destroy]
+
   def index
     @users = User.all
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: t('notice.users.delete')
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :admins
-  resources :groups
+
   devise_for :user
   get 'home/index'
   root 'home#index'
+
+  resources :admins
+  resources :groups
+  resources :users, only: [:index, :show, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
