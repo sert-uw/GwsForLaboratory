31d01ddbfa866c121922a7c062c60bb5cd799778
sert <demo.sert@gmail.com>
2015-06-22 11:32:22 +0900

    fix authority check

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,14 @@
 class AdminsController < ApplicationController
+  before_action :authority_check
+
   def index
   end
+
+  private
+
+    def authority_check
+      unless current_user.administrator?
+        redirect_to root_path, notice: t('notice.authority')
+      end
+    end
 end
